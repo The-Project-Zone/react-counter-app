0c849739a05a8248e79b54ffdf454dc26d64dc81
sricharankrishnan <sricharan.krishnan@gmail.com>
2023-01-11 10:36:32 +0530

    [Init] Initial Push
    1. created required folder structures
    2. placed required babel configs and config-overrides.js file
    3. install requied npm libraries
    4. created common stylesheet

---
+++ src/App.js
@@ -0,0 +1,25 @@
+/* node module imports */
+import React from "react";
+
+/* app imports */
+import "@components/common/css/common.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
